39706ddc3d9c69734ad0562e35806bd08f179ee5
Brad Parbs <brad@bradparbs.com>
2014-12-14 11:31:42 -0600

    1.4.1

--- Vv.rb
+++ Vv.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Vv < Formula
   homepage 'https://github.com/bradp/vv'
-  url 'https://github.com/bradp/vv/archive/1.4.0.tar.gz'
-  version '1.4.0'
-  sha1 'd173d171513dcf21470c628594ceae86cc3b3cca'
+  url 'https://github.com/bradp/vv/archive/1.4.1.tar.gz'
+  version '1.4.1'
+  sha1 'a9a620bbaba9003f71e1d72bb56067f3ce1dc11f'
 
   def install
     bin.install "vv" => "vv"
