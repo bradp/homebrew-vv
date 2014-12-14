eadaffd282df38bee1863b0eb360b362ca4ddd51
Brad Parbs <brad@bradparbs.com>
2014-12-14 11:23:02 -0600

    Explicitly define version

--- Vv.rb
+++ Vv.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Vv < Formula
   homepage 'https://github.com/bradp/vv'
   url 'https://github.com/bradp/vv/archive/1.4.0.tar.gz'
+  version '1.4.0'
   sha1 'd173d171513dcf21470c628594ceae86cc3b3cca'
 
   def install
