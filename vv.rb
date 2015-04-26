f07a532fcd2640b6e2c4accd5f289f2c8e31aff2
Brad Parbs <brad@bradparbs.com>
2015-04-26 14:38:03 -0500

    Update homebrew formula to most current version

--- vv.rb
+++ vv.rb
@@ -1,10 +1,7 @@
-require 'formula'
-
 class Vv < Formula
-  homepage 'https://github.com/bradp/vv'
-  url 'https://github.com/bradp/vv/archive/1.7.2.tar.gz'
-  version '1.7.2'
-  sha1 '5179b62bd1e88e217b27914dd083858468ac3ef9'
+  homepage "https://github.com/bradp/vv"
+  url "https://github.com/bradp/vv/archive/1.7.2.tar.gz"
+  sha256 "71cf06d22a51c05643aa87cc1f4918ffdc3bf1ca19b3341ffd0016c013f19bbd"
 
   def install
     bin.install "vv" => "vv"
