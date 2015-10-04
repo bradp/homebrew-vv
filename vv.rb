835a2c8ceebc7255d15c3bbc155909739972650d
Brad Parbs <brad@bradparbs.com>
2015-10-04 00:57:04 -0500

    1.9.3

--- vv.rb
+++ vv.rb
@@ -1,7 +1,7 @@
 class Vv < Formula
   homepage "https://github.com/bradp/vv"
-  url "https://github.com/bradp/vv/archive/1.9.2.tar.gz"
-  sha256 "66ab60e6ca068f5ea4adec7c78e1137d1fb50a985d0488950e4f8868fbe44d3f"
+  url "https://github.com/bradp/vv/archive/1.9.3.tar.gz"
+  sha256 "d119eb4b17231c7caad6950ff25dad80523adf1b2ca02412cd44a28233c275e2"
 
   def install
     bin.install "vv" => "vv"
